2f40db482658a30f8b078873c3e60ca4fc58878c
Mikko Saarinen <el4302@gmail.com>
2023-01-17 14:24:31 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -84,7 +84,6 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let h = Temporal.PlainDate.from(row.holiday);
       let holiday = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
